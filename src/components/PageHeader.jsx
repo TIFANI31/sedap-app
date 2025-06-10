4f3ecfe9724b06091d89039e4116e4f71c310ebc
TIFANI31 <tifani23ti@mahasiswa.pcr.ac.id>
2025-06-10 10:36:36 +0700

    DaisyUI

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,28 +1,80 @@
 export default function PageHeader({ title, breadcrumb, children }) {
-    return (
-      <div id="pageheader-container" className="flex items-center justify-between p-4">
-        <div id="pageheader-left" className="flex flex-col">
-          {title && (
-            <span id="page-title" className="text-3xl font-semibold">
-              {title}
-            </span>
-          )}
-          {breadcrumb && (
-            <div id="breadcrumb-links" className="flex items-center font-medium space-x-2 mt-2">
-              {typeof breadcrumb === 'string' ? (
-                <span id="breadcrumb-current" className="text-gray-500">{breadcrumb}</span>
-              ) : Array.isArray(breadcrumb) ? (
-                breadcrumb.map((item, index) => (
-                  <React.Fragment key={index}>
-                    {index > 0 && <span id="breadcrumb-separator" className="text-gray-500">/</span>}
-                    <span id={`breadcrumb-${index}`} className="text-gray-500">{item}</span>
-                  </React.Fragment>
-                ))
-              ) : null}
-            </div>
-          )}
-        </div>
-        {children && <div id="action-button">{children}</div>}
+  return (
+    <div id="pageheader-container" className="flex items-center justify-between p-4">
+      <div id="pageheader-left" className="flex flex-col">
+        {title && (
+          <span id="page-title" className="text-3xl font-semibold">
+            {title}
+          </span>
+        )}
+        {breadcrumb && (
+          // <div id="breadcrumb-links" className="flex items-center font-medium space-x-2 mt-2">
+          //   {typeof breadcrumb === 'string' ? (
+          //     <span id="breadcrumb-current" className="text-gray-500">{breadcrumb}</span>
+          //   ) : Array.isArray(breadcrumb) ? (
+          //     breadcrumb.map((item, index) => (
+          //       <React.Fragment key={index}>
+          //         {index > 0 && <span id="breadcrumb-separator" className="text-gray-500">/</span>}
+          //         <span id={`breadcrumb-${index}`} className="text-gray-500">{item}</span>
+          //       </React.Fragment>
+          //     ))
+          //   ) : null}
+          // </div>
+          <div className="breadcrumbs text-sm">
+            <ul>
+              <li>
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 stroke-current">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                  </svg>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 stroke-current">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                  </svg>
+                  Documents
+                </a>
+              </li>
+              <li>
+                <span className="inline-flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="h-4 w-4 stroke-current">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
+                  </svg>
+                  Add Document
+                </span>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-    );
-  }
+      {children && <div id="action-button">{children}</div>}
+    </div>
+  );
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -68,6 +68,49 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
+
+      <button className="btn" onClick={() => document.getElementById('my_modal_1').showModal()}>open modal</button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click the button below to close</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn m-1">Click</div>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li><a>Item 1</a></li>
+          <li><a>Item 2</a></li>
+        </ul>
+      </div>
+
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+
     </div>
+
+
+
   );
 }
+
